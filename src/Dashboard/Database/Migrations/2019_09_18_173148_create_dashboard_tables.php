385c1a05edb1931e13fa242148fa03369e92fc02
Georgi Hristov <ghristov@gmx.de>
2019-11-07 01:40:04 +0100

    [UPDATE] set dashboard table defaults

--- src/Dashboard/Database/Migrations/2019_09_18_173148_create_dashboard_tables.php
+++ src/Dashboard/Database/Migrations/2019_09_18_173148_create_dashboard_tables.php
@@ -19,15 +19,15 @@ class CreateDashboardTables extends Migration
     		$table->increments('id');
     		$table->unsignedInteger('user_id');
             $table->string('name', 64);
-            $table->smallInteger('position');
+            $table->smallInteger('position')->default(0);
         });
 
     	Schema::create('dashboard_applets', function (Blueprint $table) {
     		$table->increments('id');
     		$table->unsignedInteger('dashboard_id');
             $table->string('class', 512);            
-            $table->smallInteger('column');
-            $table->smallInteger('row');
+            $table->smallInteger('column')->default(1);
+            $table->smallInteger('row')->default(0);
             $table->text('options');
             
             $table->foreign('dashboard_id')->references('id')->on('dashboards');
